4563c0df45264f5373354a096d6eebf7eb4530e8
Chris Kenyon <cjkenyon117@gmail.com>
2018-09-17 22:27:43 -0700

    Got path finder working with BFS

--- built_robotics_challenge_1/src/path_finder.py
+++ built_robotics_challenge_1/src/path_finder.py
@@ -21,23 +21,39 @@ class PathFinder(object):
         https://docs.google.com/document/d/1b30L2LeKyMjO5rBeCui38j_HSUYgEGWXrwSRjB7AnYs/edit?usp=sharing
         """
 
-
-
-
-        ''' Naive BFS solution to validate shortest path '''
         def get_valid_neighbors(wp, visited):
-            # probably should be in waypoint
-            # TODO: comment
-            # get all the surrounding tuples to the waypoint
-            cells = starmap(lambda a,b: (wp.x+a, wp.y+b), product((0,-1,+1), (0,-1,+1)))
+            '''
+            :param wp: Waypoint
+            :return: The neighbor Waypoint(s) to Waypoint wp that have not been visited in a graph search function
+            :rtype: (Generator function) Waypoint
+            '''
+            # 0 North, 1 East, 2 South, 3 West
+            MOVES = {
+                0 : {0:(0,1),45:(-1,1),135:(-1,-1), 180:(0,-1), 225:(1,-1),  315:(1,1)},
+                1 : {0:(1,0), 45:(1,1), 135:(-1,1),180:(-1,0),225:(-1,-1), 315:(1,-1)},
+                2 : {0:(0,-1), 45:(1,-1),  135:(1,1), 180:(0,1),225:(-1,1),315:(-1,-1)},
+                3 : {0:(-1,0),45:(-1,-1), 135:(1,-1),  180:(1,0), 225:(1,1), 315:(-1,1)}
+            }
+            change_xy = MOVES[wp.orientation]
+
+            # degrees are considering forward direction as the 0 degree
+            # reference point. e.g. if north is forward, 0 degrees, then north-west is 45 degrees
+            # 90 and 270 deg motion not included (can't move left or right)
+            all_neighbors= [Waypoint(wp.x+change_xy[0][0],  wp.y+change_xy[0][1],   wp.orientation),          # 0 deg (fwr)
+                            Waypoint(wp.x+change_xy[45][0], wp.y+change_xy[45][1], (wp.orientation - 1) % 4), # 45 deg (fwr-l)
+                            Waypoint(wp.x+change_xy[135][0],wp.y+change_xy[135][1],(wp.orientation + 1) % 4), # 135 deg (bck-l)
+                            Waypoint(wp.x+change_xy[180][0],wp.y+change_xy[180][1], wp.orientation),          # 180 deg (bck)
+                            Waypoint(wp.x+change_xy[225][0],wp.y+change_xy[225][1],(wp.orientation - 1) % 4), # 225 deg (bck-r)
+                            Waypoint(wp.x+change_xy[315][0],wp.y+change_xy[315][1],(wp.orientation + 1) % 4)] # 315 deg (bck-r)
+
             # check is x in bounds, y in bounds, no obstacle
-            check = lambda w: 0 <= w[0] < grid.shape[0] and \
-                              0 <= w[1] < grid.shape[1] and \
-                              not grid[w[0], w[1]] and \
-                              (w[0],w[1]) not in visited
+            check = lambda w: 0 <= w.x < grid.shape[0] and \
+                              0 <= w.y < grid.shape[1] and \
+                              not grid[w.x, w.y] and \
+                              w not in visited
             # return a waypoint for each valid neighbor, except self (result of product 0 0)
-            for x,y in filter(check, cells):
-                yield Waypoint(x,y,0)
+            for neighbor in filter(check, all_neighbors):
+                yield neighbor
 
         def backtrack_path():
             # TODO: comment
@@ -55,16 +71,14 @@ class PathFinder(object):
         search_queue = Queue()
         start_wp.prev = None
         search_queue.put(start_wp)
-        visited = set(((start_wp.x,start_wp.y),))
+        visited = set((start_wp,))
         while not search_queue.empty():
             wp = search_queue.get()
             if wp == end_wp:
                 end_wp.prev = wp.prev
                 return backtrack_path()
             for neighbor in get_valid_neighbors(wp, visited):
-                visited.add((neighbor.x,neighbor.y))
+                visited.add(neighbor)
                 neighbor.prev = wp
                 search_queue.put(neighbor)
-        #pdb.set_trace()
         return []
-        ''' '''
